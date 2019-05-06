ce741810d2ede472a4f1d63840d6a10ca4684d1e
Tonni Tielens <tonnitielens@gmail.com>
2019-05-06 20:29:30 +0200

    Add sprout method and class example

---
+++ examples/SproutMethod/SproutMethod/FileUpdateManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SproutMethod
+{
+    public class FileUpdateManager
+    {
+        public void AppendFilesWithText(IList<string> filePaths, string textToAppend)
+        {
+            foreach (var file in filePaths)
+            {
+                if (File.Exists(file) && string.IsNullOrEmpty(textToAppend))
+                {
+                    File.AppendAllText(file, textToAppend);
+                }
+            }
+            NotifyThatFilesHaveChangedByEmail(filePaths);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void NotifyThatFilesHaveChangedByEmail(IList<string> filePath)
+        {
+            // some very complex implementation
+        }
+    }
+}
